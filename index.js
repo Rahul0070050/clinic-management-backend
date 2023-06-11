1cdc0c8c6cb3682d8387c5143b393eb39c985952
rahul <rahulor2000@gmail.com>
2023-06-11 12:20:32 +0530

    cors origin chenged

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const adminRoutes = require("./src/routes/adminRoutes");
 const app = express()
 
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["*"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true
 }))
